e3fca525a0528e36bdaec95ff7db89f02cc56939
bandapallyvaishnavi <167325507+bandapallyvaishnavi@users.noreply.github.com>
2024-04-29 14:04:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+*{
+    font-family = 'open sans',sans-serif;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    background-size: felx;
+   
+}
+
+.container{
+    background-color: #fff;
+    margin-top: 20px;
+    width: 350px;
+    box-shadow: 0px 0px 20px 10px #0044ff;
+    padding: 50px;
+    border-radius: 30px;
+    text-align: centre;
+}
+h1{
+    margin-bottom: 40px;
+}
+input{
+  
+    border: none;
+    border-bottom: 2px solid #0044ff;
+    width: 50%;
+    margin-bottom: 20px;
+    outline: none
+    padding : 10px;
+    text-align: center;
+    font-size: 25px;
+}
+button{
+    width: 100%;
+    height: 35px;
+    margin: 25px;
+    border-radius: 20px;
+    border: none;
+    background-color: #0044ff;
+    color: #fff;
+    font-size: 22px;
+
+
+}
+#result{
+    margin-top: 20px;
+    font-size: 25px;
+}
